9be7f66f339bd3dfce22ba8478350531e543dbdd
Mary Angeline Samson <97255743+AngieS-git@users.noreply.github.com>
2022-06-23 16:40:18 +0800

    Add files via upload
    select function works

---
+++ LR1/main.py
@@ -0,0 +1,24 @@
+def primary():
+     #inputFN = input("Enter the file name: " )
+    
+
+     with open(r"G:\MAPUA\2nd Year\4th Term\CPE106L\Module 1\LR1 PP\CPE106L_E01_4Q2122\LR1\text.txt", 'r') as fp:
+          for count, line in enumerate(fp):
+               pass
+          num_lines = count+1
+          print('Total lines:', num_lines) # 8
+     
+     EnterVal = int(input("Enter Value to select a line: "))
+
+     if EnterVal!=0:
+          f = open(r"G:\MAPUA\2nd Year\4th Term\CPE106L\Module 1\LR1 PP\CPE106L_E01_4Q2122\LR1\text.txt","r")
+          quotes = f.readlines()
+          print(quotes[EnterVal-1])
+          f.close()
+     else:
+          print("You have exited the program :)")
+
+
+if __name__== "__main__":
+  primary()
+
