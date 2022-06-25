ea90edaec7f7f184a32954102c509cfeba2db52a
Patrick Howard Yango <yangohy88@gmail.com>
2022-06-25 19:58:21 +0800

    Uploaded revised problem 2

--- LR1/main.py
+++ LR1/main.py
@@ -1,19 +1,47 @@
 def primary():
-     #inputFN = input("Enter the file name: " )
+     inputFN = input("Enter the file name [include .txt]: " )
+     line_assign = int(input("Enter the number of lines you wish to put in the .txt file: "))
+     lines = []
 
-     with open(r"C:\Users\patri\Documents\3rd Year\4th Sem\CPE106L\LR\LR1\P2\CPE106L_E01_4Q2122\LR1\text.txt", 'r') as fp:
-          num_lines = sum(1 for line in fp)
-          print('Total lines:', num_lines) # 8
-    
+     if(line_assign != 0):
+          f = open(inputFN, "w+")
+          
 
-     if num_lines!=0:
-          print("Enter Value: ")
-          quotes=input()
-          print(quotes[num_lines])
+          for i in range(line_assign):
+               arr = input()
+               lines.append(arr)
+
+          for i in range(line_assign):
+               f.write(lines[i])
+               f.write("\n")
+          
+          f = open(inputFN, "r")
+          readline = f.readlines()
+
+          print("\n\nThe inputted lines are: \n")
+
+          for y in range(line_assign):
+               print(readline[y])
+          
+          f.close()
+
+          print("Please pick a line number from the list of lines shown above: ")
+          EnterVal = int(input())
+          
+          if EnterVal!=0 and EnterVal > line_assign:
+               print("The inputted line number must be between 1 and ", line_assign)
+               
+          elif EnterVal!=0 and EnterVal<= line_assign:
+                    f = open(inputFN,"r")
+                    quotes = f.readlines()
+                    print("Line","[",EnterVal, "]:",quotes[EnterVal-1])
+                    f.close()
+          else:
+                    print("You have exited the program :)")
      else:
           print("You have exited the program :)")
-
-
+     
+          
+    
 if __name__== "__main__":
   primary()
-
